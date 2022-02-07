81189191edcb71d2e50eb907f6971be7e3e456ee
halsaylani <hasooo7y@hotmail.com>
2022-02-07 11:45:38 +0300

    Update ListActivity.kt

--- app/src/main/java/com/averos/als/positioningdemo/ListActivity.kt
+++ app/src/main/java/com/averos/als/positioningdemo/ListActivity.kt
@@ -26,6 +26,8 @@ import java.util.HashMap
 class ListActivity : AppCompatActivity() {
 
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.my_attandance_list)
@@ -33,8 +35,8 @@ class ListActivity : AppCompatActivity() {
         var userName = SharedPrefManager.getInstance(this).user.name
         var recyclerView = findViewById<RecyclerView>(R.id.recycle)
 
-        print("kotlin respons Email is ======> $userEmail")
-        print("kotlin respons Name is ======> $userName")
+        print(userEmail)
+        print(userName)
 
         //////////toolbar//////////////////
         var toolbar = findViewById<View>(R.id.main_toolbar) as Toolbar
@@ -57,117 +59,111 @@ class ListActivity : AppCompatActivity() {
             //window.statusBarColor = this.resources.getColor(R.color.border_color)
         }
 
-//        //recyclerView.setBackgroundColor(Color.BLUE)
-//        recyclerView.layoutManager = LinearLayoutManager(this)
-//        //recyclerView.adapter = MainAdapter()
-//
-//
-//
-//
-//            //final TextView textView = (TextView) findViewById(R.id.respon);
-//            //if everything is fine
-//            val stringRequest: StringRequest = object : StringRequest(
-//                Method.GET, URLs.URL_ATTENDANCE + LoginActivity.authResult.getUser().displayableId,
-//                com.android.volley.Response.Listener { response ->
-//                    //progressBar.setVisibility(View.VISIBLE);
-//                    try {
-//                        if (response.isNotEmpty()) {
-//                            val body = response.toString();
-//                            println("this is body"+body)
-//                            val jObject =  JSONArray(body)
-//                            println("this is body one"+jObject[1].toString())
-//
-//                            val homeFeed = jObject
-//                            println(homeFeed)
-//                            runOnUiThread{
-//                                recyclerView.adapter =MainAdapter(homeFeed)
-//                            }
-//
-//
-//                        }else {
-//                            Toast.makeText(
-//                                applicationContext,
-//                                "no idea",
-//                                Toast.LENGTH_SHORT
-//                            ).show()
-//                        }
-//                    } catch (e: JSONException) {
-//                        e.printStackTrace()
-//                    }
-//                },
-//                com.android.volley.Response.ErrorListener { error ->
-//                    Toast.makeText(
-//                        applicationContext,
-//                        error.message,
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                }) {
-//                @Throws(AuthFailureError::class)
-//                override fun getHeaders(): Map<String, String> {
-//                    val headers = HashMap<String, String>()
-//                    print("this from header token${SharedPrefManager.getInstance(getApplicationContext()).token.token}")
-//                    headers["Content-Type"] = "application/json; charset=utf-8"
-//                    headers["Authorization"] = "Bearer${SharedPrefManager.getInstance(getApplicationContext()).token.token}"
-//                    return headers
-//                }
-//            }
-//            VolleySingleton.getInstance(this).addToRequestQueue(stringRequest)
-//
-//
-//        val client = OkHttpClient()
-//        println("user id is "+LoginActivity.userID)
-//        //"https://attend.ksauhs.com/api/attendance/user/" + LoginActivity.userID
-//        val url =
-//            HttpUrl.parse(URLs.URL_ATTENDANCE + LoginActivity.authResult.getUser().displayableId)!!
-//                .newBuilder()
-//                .build()
-//        val request = Request.Builder()
-//            .url(url)
-//            .method("GET", null)
-//            .build()
-//        val call = client.newCall(request)
-//        call.enqueue(object : Callback {
-//            @kotlin.jvm.Throws(IOException::class)
-//            override fun onResponse(call: Call, response: Response) {
-//                if (response.isSuccessful) {
-//                    val body = response.body()!!.string()
-//                    println("this is body"+body)
-//                    val jObject =  JSONArray(body)
-//                    println("this is body one"+jObject[1].toString())
-//
-//                    val homeFeed = jObject
-//                    println(homeFeed)
-//                    runOnUiThread{
-//                        recyclerView.adapter =MainAdapter(homeFeed)
-//                    }
-//
-//
-//                }
-//            }
-//
-//            override fun onFailure(call: Call, e: IOException) {
-//                runOnUiThread {
-//                    val toast =
-//                        Toast.makeText(this@ListActivity, "failed to fetch api", Toast.LENGTH_LONG)
-//                    toast.show()
-//                }
-//                println("failed to fetch api")
-//            }
-//        })
-//    }
-    // don't forget click listener for back button
+        //recyclerView.setBackgroundColor(Color.BLUE)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        //recyclerView.adapter = MainAdapter()
 
 
 
+
+            //final TextView textView = (TextView) findViewById(R.id.respon);
+            //if everything is fine
+            val stringRequest: StringRequest = object : StringRequest(
+                Method.GET, URLs.URL_ATTENDANCE + LoginActivity.authResult.getUser().displayableId,
+                com.android.volley.Response.Listener { response ->
+                    //progressBar.setVisibility(View.VISIBLE);
+                    try {
+                        if (response.isNotEmpty()) {
+                            val body = response.toString();
+                            println("this is body"+body)
+                            val jObject =  JSONArray(body)
+                            println("this is body one"+jObject[1].toString())
+
+                            val homeFeed = jObject
+                            println(homeFeed)
+                            runOnUiThread{
+                                recyclerView.adapter =MainAdapter(homeFeed)
+                            }
+
+
+                        }else {
+                            Toast.makeText(
+                                applicationContext,
+                                "no idea",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+                },
+                com.android.volley.Response.ErrorListener { error ->
+                    Toast.makeText(
+                        applicationContext,
+                        error.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }) {
+                @Throws(AuthFailureError::class)
+                override fun getHeaders(): Map<String, String> {
+                    val headers = HashMap<String, String>()
+                    print("this from header token${SharedPrefManager.getInstance(getApplicationContext()).token.token}")
+                    headers["Content-Type"] = "application/json; charset=utf-8"
+                    headers["Authorization"] = "Bearer${SharedPrefManager.getInstance(getApplicationContext()).token.token}"
+                    return headers
+                }
+            }
+            VolleySingleton.getInstance(this).addToRequestQueue(stringRequest)
+
+
+        val client = OkHttpClient()
+        println("user id is "+LoginActivity.userID)
+        //"https://attend.ksauhs.com/api/attendance/user/" + LoginActivity.userID
+        val url =
+            HttpUrl.parse(URLs.URL_ATTENDANCE + LoginActivity.authResult.getUser().displayableId)!!
+                .newBuilder()
+                .build()
+        val request = Request.Builder()
+            .url(url)
+            .method("GET", null)
+            .build()
+        val call = client.newCall(request)
+        call.enqueue(object : Callback {
+            @kotlin.jvm.Throws(IOException::class)
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    val body = response.body()!!.string()
+                    println("this is body"+body)
+                    val jObject =  JSONArray(body)
+                    println("this is body one"+jObject[1].toString())
+
+                    val homeFeed = jObject
+                    println(homeFeed)
+                    runOnUiThread{
+                        recyclerView.adapter =MainAdapter(homeFeed)
+                    }
+
+
+                }
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                runOnUiThread {
+                    val toast =
+                        Toast.makeText(this@ListActivity, "failed to fetch api", Toast.LENGTH_LONG)
+                    toast.show()
+                }
+                println("failed to fetch api")
+            }
+        })
     }
+    // don't forget click listener for back button
     override fun onSupportNavigateUp(): Boolean {
-        var userEmail = SharedPrefManager.getInstance(this).user.email
-        var userName = SharedPrefManager.getInstance(this).user.name
-        print("kotlin respons Email is ======> $userEmail")
-        print("kotlin respons Name is ======> $userName")
         onBackPressed()
         return true
     }
+
+
 }
 
 
